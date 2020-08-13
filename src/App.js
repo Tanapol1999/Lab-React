b27a99880dae7b5fd09b419849344d5544ce18d6
Tanapol Commit <6110110178@psu.ac.th>
2020-08-13 19:10:30 +0700

    Part4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import CharacterCard from './CharacterCard';
 import React from 'react';
 import './App.css'
+import WordCard from './WordCard';
+
+const word = "Hello";
 
 function App() {
     return (
-
        <div>
-           <CharacterCard value = "h" />
-           <CharacterCard value = "i" />
+           {
+            <WordCard value = {word}/>
+           }
        </div>
     );
 }
 
 export default App;
-
-// import React from 'react';
-// import './App.css';
-// function App() {
-//  return (
-//  <div>
-//  Hello World
-//  </div>
-//  );
-// }
-// export default App;
