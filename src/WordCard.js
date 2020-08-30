abd500f1b7497a4e25fa547ce682b273e40c00c3
Tanapol Commit <6110110178@psu.ac.th>
2020-08-30 21:23:05 +0700

    Part 7.7 Debug and complete lose condition

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import CharacterCard from './CharacterCard';
 import _, { attempt } from 'lodash';
 
 var answer = "";
-var score = 0;
+//var score = 0;
 var temp = 0;
 
 const prepareStateFromWord = given_word => {
@@ -18,73 +18,81 @@ const prepareStateFromWord = given_word => {
         completed: false
     }
 }
+/*const prepareNewWord = given_word =>{
+    let words = given_word.toUpperCase()
+    words.toUpperCase()
+    let chars2 = _.shuffle(Array.from(words))
+    return (
+        <div class="center">
+        {
+            chars2.map((c, i) => <CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt} />)
+        }
+        </div>
+    )
+}*/
 
-export default function WordCard(props){
+export default function WordCard(props) {
 
     const [state, setState] = useState(prepareStateFromWord(props.value))
-    
+
     answer = state.guess;
 
     const activationHandler = c => {
         console.log(`${c} has been activated.`)
         let guess = state.guess + c
-        setState({...state, guess})
+        setState({ ...state, guess })
 
-        if(guess.length == state.word.length){
-            if(guess == state.word){
+        if (guess.length == state.word.length) {
+            if (guess == state.word) {
                 console.log('Yeah!')
-                setState({...state, guess: '', completed: true})
-                setState({...state,guess:'',attempt: state.attempt + 1})
+                setState({ ...state, guess: '', completed: true })
+                setState({ ...state, guess: '', attempt: state.attempt + 1 })
                 alert("You won!!");
-                score++;    
+                //score++;
                 temp = 0;
-
-                    if(score == 5){
-                        alert("Congrat!! You win all!!");
-                        window.location.reload(false);
-                    }
-                //window.location.reload(false);
-            }else{
+                
+                // if (score == 5) {
+                //     alert("Congrat!! You win all!!");
+                //     window.location.reload(false);
+                // }
+                window.location.reload(false);
+            } else {
                 console.log('reset, next attempt')
-                setState({...state,guess:'',attempt: state.attempt + 1})
+                setState({ ...state, guess: '', attempt: state.attempt + 1 })
                 alert("Incorrect, Please Try Again!!")
                 temp++;
-                
-                    if(temp == 3){
+
+                    if (temp == 3) {
                         alert("GAME OVER!! Please Try Again");
                         window.location.reload(false);
                     }
             }
         }
-        console.log({temp});
+        console.log({ temp });
         state.attempt += 1;
     }
 
-    return(
+    return (
         <div>
             <br></br>
             <br></br>
-            <div class = "font0">
+            <div class="font0">
                 <b>Guess Province In Thailand</b>
             </div>
-            
+
             <div class="center">
                 {
-                    state.chars.map((c, i) => <CharacterCard value={c} key={i} activationHandler = {activationHandler} attempt = {state.attempt}/>)   
+                    state.chars.map((c, i) => <CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt} />)
                 }
             </div>
             <br></br>
             <br></br>
-            <div class ="font1">
+            <div class="font1">
                 <b>Your Answer :</b>  {answer}
             </div>
-            <div class ="font1">
+            <div class="font1">
                 <b>Your Attemp :</b> {temp}
             </div>
-            <div class ="font1">
-                <b>Your Score :</b>  {score}
-            </div>
-            
         </div>
     );
 }
